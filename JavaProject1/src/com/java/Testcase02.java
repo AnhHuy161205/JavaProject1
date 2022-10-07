8c57f26d9235075b59ea9097954ea535963ccc44
anhhu <anhhu@DESKTOP-EFLBFUE>
2022-10-07 10:35:12 +0700

    Tira edit Testcase 02

--- JavaProject1/src/com/java/Testcase02.java
+++ JavaProject1/src/com/java/Testcase02.java
@@ -3,7 +3,7 @@ package com.java;
 public class Testcase02 {
 
 	public static void main(String[] args) {
-		System.out.println("Testcase 02");
+		System.out.println("Testcase 02 là của Tira");
 
 	}
 
